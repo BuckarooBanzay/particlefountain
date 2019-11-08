c85f2f87caeeff76ecd0e0dbc278b5a2430720c3
BuckarooBanzay <BuckarooBanzay@github.com>
2019-11-08 11:55:59 +0100

    initial draft

---
+++ init.lua
@@ -0,0 +1,122 @@
+
+local update_formspec = function(meta)
+	local pos = meta:get_string("pos")
+
+	meta:set_string("infotext", "Add item block: pos=" .. pos)
+
+	meta:set_string("formspec", "size[8,7;]" ..
+		"field[0.2,0.5;4,1;amount;Amount;${amount}]" ..
+		"field[4.2,0.5;4,1;amount;Amount;${amount}]" ..
+
+		"button_exit[6.1,1.5;2,1;save;Save]" ..
+		"list[context;main;0.1,1.5;1,1;]" ..
+
+		"list[current_player;main;0,3;8,4;]" ..
+		"listring[]" ..
+		"")
+end
+
+minetest.register_node("particlefountain:particlefountain", {
+	description = "Particlefountain",
+	tiles = {
+		"default_gold_block.png^default_mese_crystal.png"
+	},
+	groups = {cracky=3,oddly_breakable_by_hand=3},
+
+	on_construct = function(pos)
+    local meta = minetest.get_meta(pos)
+		meta:set_int("amount", 4)
+		meta:set_int("glow", 9)
+
+		local inv = meta:get_inventory()
+		inv:set_size("main", 1)
+
+    update_formspec(meta, pos)
+		minetest.get_node_timer(pos):start(2)
+  end,
+
+  on_receive_fields = function(pos, _, fields, sender)
+		if not sender or minetest.is_protected(pos, sender:get_player_name()) then
+			-- not allowed
+			return
+		end
+
+		local meta = minetest.get_meta(pos);
+
+		if fields.save then
+			meta:set_int("amount", tonumber(fields.amount) or 4)
+			meta:set_int("glow", tonumber(fields.glow) or 9)
+		end
+
+		update_formspec(meta)
+  end,
+
+	allow_metadata_inventory_put = function(pos, _, _, stack, player)
+		if minetest.is_protected(pos, player:get_player_name()) then
+			return 0
+		end
+
+		return stack:get_count()
+	end,
+
+	allow_metadata_inventory_take = function(pos, _, _, stack, player)
+		if minetest.is_protected(pos, player:get_player_name()) then
+			return 0
+		end
+
+		return stack:get_count()
+	end,
+
+	on_timer = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		local stack = inv:get_stack("main", 1)
+		local node_name = stack:get_name()
+		if node_name == "ignore" or node_name == "" then
+			return true
+		end
+
+		local def = minetest.registered_items[node_name]
+		if not def then
+			return true
+		end
+
+		local texture = "default_mese_crystal.png"
+
+		if def.inventory_image then
+			texture = def.inventory_image
+
+		elseif def.tiles then
+			if type(def.tiles) == "string" then
+				texture = def.tiles
+
+			elseif type(def.tiles) == "table" then
+				texture = def.tiles[1]
+
+			end
+		end
+
+		minetest.add_particlespawner({
+			amount = meta:get_int("amount"),
+			time = 2,
+			minpos = vector.add(pos, {x=-0.2, y=0, z=-0.2}),
+			maxpos = vector.add(pos, {x=0.2, y=0, z=0.2}),
+			minvel = {x=0, y=1, z=0},
+			maxvel = {x=0, y=2, z=0},
+			minacc = {x=0, y=0, z=0},
+			maxacc = {x=0, y=0, z=0},
+			minexptime = 1,
+			maxexptime = 2,
+			minsize = 1,
+			maxsize = 1.7,
+			collisiondetection = false,
+			collision_removal = false,
+			object_collision = false,
+			vertical = false,
+			texture = texture,
+      glow = meta:get_int("glow")
+		})
+
+		return true
+	end
+})
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+particlefountain
+-----------------
+
+A mod for [minetest](http://www.minetest.net)
+
+[![Build Status](https://travis-ci.org/damocles-minetest/particlefountain.svg?branch=master)](https://travis-ci.org/damocles-minetest/particlefountain)
+
+# Overview
+
+decorative and configurable particle spawner
+
+# License
+
+## Code
+
+* MIT
